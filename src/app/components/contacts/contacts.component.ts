91a2229171c5d4c347bec373c57cf3f5d62eb1e4
bhargav-5 <nagetibhargavram@gmail.com>
2020-02-26 18:32:22 +0530

    commit

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Pages } from "../..//entities/pages";
+// import { Pages } from "../..//entities/pages";
 import { NgForm } from "@angular/forms";
+import { Designation } from "src/app/entities/Designation";
 
 @Component({
   selector: "app-contacts",
   templateUrl: "./contacts.component.html",
   styleUrls: ["./contacts.component.css"]
 })
-export class ContactsComponent implements OnInit { 
-  pages: Pages;
+export class ContactsComponent implements OnInit {
+  // pages: Pages;
+
+  // constructor() {
+  //   this.pages = new Pages();
+  // }
+
+  // ngOnInit() {}
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // }
+  designation: Designation;
 
   constructor() {
-    this.pages = new Pages();
+    this.designation = new Designation();
   }
 
   ngOnInit() {}
-
-  onSubmit(form: NgForm) {
-    console.log(form.value);
+  submit() {
+    console.log(this.designation);
   }
 }
---
+++ src/app/entities/Designation.ts
@@ -0,0 +1,11 @@
+export class Designation {
+  id: string;
+  brand: string;
+  name: string;
+  jobLevel: number;
+  summary: string;
+  parent: string;
+  vid: string;
+  updatedBy: string;
+  updatedOn: Date;
+}
